1fec7fd38518e6195a7f2c79088c82f9f7e8caa1
Julian Nicholls <juliannicholls29@gmail.com>
2015-05-31 19:06:06 +0100

    Added a remembering facility

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
 
   before_save { self.email.downcase! }
 
@@ -21,5 +22,31 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Creates a new remembral token
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remember a user in the database for use in persistent sessions
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Forget a user when logged out
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Returns whether the given token matches the digest
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
   end
 end
