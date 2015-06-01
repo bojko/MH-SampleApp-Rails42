e127ad1c9a53cf0099fc0ff5de11fdbd73a37b09
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-01 20:38:47 +0100

    Added redirection back after enforced login.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -55,4 +55,15 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  # Redirection to stored location or default
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete :forwarding_url
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
