1844f3c5d8a7e09496d653561a92c2f5ac75e30c
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-03 20:22:05 +0100

    Added and tested activation emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,9 +46,23 @@ class User < ActiveRecord::Base
 
   # Returns whether the given token matches the digest
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated?(attribute, token)
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  # Take care of activation
+
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Send the activation email
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
