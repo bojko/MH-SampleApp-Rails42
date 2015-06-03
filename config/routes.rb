1a0cb00ed427c1146a43b9d7cca4526a590108bf
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-03 18:29:42 +0100

    Added activation fields, and updated seedings.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :users
-
   root                 'static_pages#home'
 
   get     'help'    => 'static_pages#help'
@@ -13,5 +11,8 @@ Rails.application.routes.draw do
   get     'login'   => 'sessions#new'
   post    'login'   => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
+
+  resources :users
+  resources :account_activations, only: [:edit]
 
 end
