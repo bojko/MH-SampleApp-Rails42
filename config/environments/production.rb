0a5d5cf879a17eb9819b636c2558d81def1e868e
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-05 18:15:35 +0100

    One last go at the SendGrid mailer settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'julian-mh-sample.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+
   ActionMailer::Base.smtp_settings = {
-    address:                'smtp.sendgrid.net',
-    port:                   '587',
-    authentication:         :plain,
-    user_name:              ENV['SENDGRID_USERNAME'],
-    password:               ENV['SENDGRID_PASSWORD'],
-    domain:                 'heroku.com',
-    enable_starttls_auto:   true
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
 
