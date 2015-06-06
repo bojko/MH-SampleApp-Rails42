3b8f6b13916629e38eebac63afa84d1b217169b4
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-06 21:37:41 +0100

    Added Microposts model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save     :downcase_email
@@ -18,7 +20,6 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string
-
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -26,26 +27,22 @@ class User < ActiveRecord::Base
   end
 
   # Creates a new remembral token
-
   def self.new_token
     SecureRandom.urlsafe_base64
   end
 
   # Remember a user in the database for use in persistent sessions
-
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   # Forget a user when logged out
-
   def forget
     update_attribute(:remember_digest, nil)
   end
 
   # Returns whether the given token matches the digest
-
   def authenticated?(attribute, token)
     digest = send "#{attribute}_digest"
     return false if digest.nil?
@@ -53,26 +50,22 @@ class User < ActiveRecord::Base
   end
 
   # Take care of activation
-
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # Send the activation email
-
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
 
   # Sets the password reset attributes
-
   def create_reset_digest
     self.reset_token = User.new_token
     update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # Send the password reset email
-
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
