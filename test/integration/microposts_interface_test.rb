34071d8f5bfe7fe7b967ffc61dfc79e9545d4559
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-08 16:50:06 +0100

    Refactored home page into partials.
    Added tests for the microposts on the homepage.

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as @user
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     # invalid submission
     assert_no_difference 'Micropost.count' do
@@ -18,9 +19,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Valid submission
     content = 'This micropost really ties the room together'
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -28,6 +31,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Delete a post
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
+    assert first_micropost.picture?
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
@@ -35,5 +39,22 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Visit a different user
     get user_path(users :archer)
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "Micropost count" do
+    log_in_as @user
+    get root_path
+    assert_match "34 microposts", response.body
+
+    # User with 0 posts
+    other_user = users :mallory
+    log_in_as other_user
+    get root_path
+    assert_match "0 microposts", response.body
+
+    # So add one
+    other_user.microposts.create(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
