f2860ddf890958fc87d2ea159a56d071ab1b2ced
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-09 18:41:26 +0100

    Set up followed / following relationships.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -91,4 +91,19 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+# Following
+
+  test "should follow and unfollow other users" do
+    michael = users :michael
+    archer  = users :archer
+
+    assert_not michael.following? archer
+    michael.follow archer
+    assert michael.following? archer
+    assert archer.followers.include? michael
+
+    michael.unfollow archer
+    assert_not michael.following? archer
+  end
 end
