ed6b659b1eb9bac8f9c3925421537afbb7277fe9
Julian Nicholls <juliannicholls29@gmail.com>
2015-06-10 20:36:27 +0100

    The feed now includes microposts from all followed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -88,7 +88,8 @@ class User < ActiveRecord::Base
 
   # Set up a micropost feed
   def feed
-    Micropost.where('user_id = ?', id)
+    following_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # Follow another user
